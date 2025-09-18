277163498a4de0e1467cc414116aa4726218c930
Michael Mulet <m@mmulet.com>
2025-09-17 22:44:47 -0500

    fixed right mouse support, and sub windows

--- src/Canvas_Desktop.ts
+++ src/Canvas_Desktop.ts
@@ -11,6 +11,8 @@ import { wl_surface } from "./objects/wl_surface.ts";
 import { Buffer } from "buffer";
 //@ts-ignore
 import icon from "../resources/icon.png" with { type: "file" };
+import { Object_ID } from "./wayland_types.ts";
+import { wl_surface as wl_surface_protocol } from "./protocols/wayland.xml.ts";
 
 export class Canvas_Desktop {
   canvas: Canvas;
@@ -56,7 +58,20 @@ export class Canvas_Desktop {
      * Do z sorting
      * of all drawable surfaces
      */
-    const sorted_surfaces: [wl_surface, Canvas][] = [];
+    const sorted_surfaces: [
+      wl_surface,
+      Canvas,
+      Object_ID<wl_surface_protocol>,
+    ][] = [];
+
+    const child_to_parent_map: {
+      [child_id: Object_ID<wl_surface_protocol>]: {
+        parent_id: Object_ID<wl_surface_protocol>;
+        x: number;
+        y: number;
+      };
+    } = {};
+
     for (const s of clients) {
       for (const surface_id of s.drawable_surfaces) {
         const surface = s.get_object(surface_id)?.delegate;
@@ -69,8 +84,18 @@ export class Canvas_Desktop {
         if (!surface.texture.canvas) {
           continue;
         }
+        for (const child of surface.children_in_draw_order) {
+          if (!child) {
+            continue;
+          }
+          child_to_parent_map[child] = {
+            parent_id: surface_id,
+            x: surface.position.x,
+            y: surface.position.y,
+          };
+        }
 
-        sorted_surfaces.push([surface, surface.texture.canvas]);
+        sorted_surfaces.push([surface, surface.texture.canvas, surface_id]);
       }
     }
     sorted_surfaces.sort((a, b) => {
@@ -83,8 +108,21 @@ export class Canvas_Desktop {
       this.draw_image(this.icon_image, 0, 0);
     }
 
-    for (const [surface, texture] of sorted_surfaces) {
-      this.context.drawImage(texture, surface.position.x, surface.position.y);
+    for (const [surface, texture, surface_id] of sorted_surfaces) {
+      /**
+       * Recursively get the position by adding
+       * all ancestor position
+       */
+      let x = surface.position.x;
+      let y = surface.position.y;
+      let parent = child_to_parent_map[surface_id];
+      while (parent != undefined) {
+        x += parent.x;
+        y += parent.y;
+        parent = child_to_parent_map[parent.parent_id];
+      }
+
+      this.context.drawImage(texture, x, y);
     }
   };
 }
